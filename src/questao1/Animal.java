c7890b48c15586005e4d1da88b3d420da02f473b
Vinícius <vinicius.bornhofen47@gmail.com>
2025-08-28 21:47:30 -0300

    falta so o main

---
+++ src/questao1/Animal.java
@@ -0,0 +1,116 @@
+package questao1;
+
+/**
+ * Classe representa um animal
+ * 
+ * @author Vinícius Bornhofen (vinbornhofen@furb.br)
+ */
+public class Animal {
+    private String nome;
+    private String especie;
+    private int idade;
+    private String historico;
+
+    /**
+     * Este é o construtor padrão do Animal,
+     * inicia o nome como "Nome de Animal Desconhecido",
+     * a especie como "Espécie de Animal Desconhecido",
+     * a idade como 1 ano,
+     * e o histórico como "Historico de Animal Desconhecido"
+     */
+    public Animal() {
+        nome = "Nome de Animal Desconhecido";
+        especie = "Espécie de Animal Desconhecido";
+        idade = 1;
+        historico = "Historico de Animal Desconhecido";
+    }
+
+    /**
+     * Este é o construtor completo do Animal,
+     * inicia o nome como o parametro String nome,
+     * a especie como o parametro String especie,
+     * a idade como o parametro int idade,
+     * e o histórico como o parametro String historico
+     * @param nome, o nome
+     * @param especie, a especie
+     * @param idade, a idade
+     * @param historico, o histórico
+     */
+
+    public Animal(String nome, String especie, int idade, String historico) {
+        this.nome = nome;
+        this.especie = especie;
+        this.idade = idade;
+        this.historico = historico;
+    }
+
+    /**
+     * método getter do nome
+     * @return o nome do animal
+     */
+    public String getNome(){
+        return nome;
+    }
+    
+    /**
+     * método getter da especie
+     * @return a especie do animal
+     */
+    public String getEspecie(){
+        return especie;
+    }
+
+    /**
+     * método getter da idade
+     * @return a idade do animal
+     */
+    public int getIdade(){
+        return idade;
+    }
+
+    /**
+     * método getter do historico
+     * @return o historico do animal
+     */
+    public String getHistorico(){
+        return historico;
+    }
+
+    /**
+     * método setter do nome
+     * @param o nome do animal
+     */
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    /**
+     * método setter da especie
+     * @param a especie do animal
+     */
+    public void setEspecie(String especie){
+        this.especie = especie;
+    }
+
+    /**
+     * método setter da idade
+     * @param a idade do animal
+     * @throws IllegalArgumentException se idade for negativa
+     */
+    public void setIdade(int idade){
+        if(idade < 0){
+             throw new IllegalArgumentException("Idade não pode ser negativa!");
+        }
+        this.idade = idade;
+    }
+
+    /**
+     * método setter do historico
+     * @param o historico do animal
+     */
+    public void setHistorico(String historico){
+        this.historico = historico;
+    }
+
+
+}
---
+++ src/questao1/Tutor.java
@@ -0,0 +1,92 @@
+package questao1;
+/**
+ * Classe representa um tutor
+ * 
+ * @author Vinícius Bornhofen (vinbornhofen@furb.br)
+ */
+public class Tutor {
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+     /**
+     * Este é o construtor completo do Tutor,
+     * inicia o nome como o parametro String nome,
+     * o telefone como o parametro String telefone,
+     * o endereco como o parametro String endereco,
+     * @param nome, o nome
+     * @param telefone, o telefone
+     * @param endereco, o endereco
+     */
+    public Tutor(String nome, String telefone, String endereco){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+      /**
+     * Este é o construtor padrão do Tutor,
+     * inicia o nome como "Nome Desconhecido",
+     * e o endereco como "Endereço Desconhecido"
+     */
+    public Tutor(){
+        nome = "Nome desconhecido";
+        endereco = "Endereço Desconhecido";
+    }
+
+    /**
+     * metodo getter do nome
+     * @return nome do tutor
+     */
+    public String getNome(){
+        return nome;
+    }
+
+    /**
+     * metodo getter do telefone
+     * @return telefone do tutor
+     */
+    public String getTelefone(){
+        return telefone;
+    }
+
+    /**
+     * metodo getter do endereco
+     * @return endereco do tutor
+     */
+    public String getEndereco(){
+        return endereco;
+    }
+
+    
+    /**
+     * método setter do nome
+     * @param o nome do tutor
+     */
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    /**
+     * método setter do telefone
+     * @param o telefone do tutor
+     * @throws IllegalArgumentException caso o telefone seja null
+     */
+    public void setTelefone(String telefone){
+        if(telefone == null){
+            throw new IllegalArgumentException("Telefone não pode ser nullo!");
+        }
+        this.telefone = telefone;
+    }
+
+    /**
+     * método setter do endereco
+     * @param o endereco do tutor
+     */
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+
+
+}
